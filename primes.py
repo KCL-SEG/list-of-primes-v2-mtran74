deaed193647a3627090f525f8dd4eb731d898388
Mathew Tran <m-mathew@hotmail.co.uk>
2022-10-13 19:38:13 +0100

    Prime v1

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes < 1:
+        raise ValueError("Incorrect value entered try again")
+        return
+    list = [2]
+    counter = 3
+    while len(list) != number_of_primes:
+        prime = True
+        for i in range(3, counter + 1):
+            if counter % i == 0:
+                prime = False
+
+        if prime == True:
+            list.append(i)
+        counter += 2
     return list
