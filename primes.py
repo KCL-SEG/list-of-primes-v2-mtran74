6b9ccd559e5ee8746a8255f870f44acf9bad95e9
Mathew Tran <m-mathew@hotmail.co.uk>
2022-12-08 20:21:29 +0000

    List of Primes

--- primes.py
+++ primes.py
@@ -5,15 +5,15 @@ def primes(number_of_primes):
     if number_of_primes < 1:
         raise ValueError("Incorrect value entered try again")
         return
-    list = [2]
-    counter = 3
-    while len(list) != number_of_primes:
-        prime = True
-        for i in range(3, counter + 1):
-            if counter % i == 0:
-                prime = False
-
-        if prime == True:
-            list.append(i)
-        counter += 2
+    list = []
+    nextPrime = 2
+    while len(list) < number_of_primes:
+        is_prime = True
+        for i in range(2, nextPrime):
+            if nextPrime % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            list.append(nextPrime)
+        nextPrime += 1
     return list
